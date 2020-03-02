ed89094f5a0c68e7d4ff674026836dda4d77fd03
Koberskiy <kober_12@ukr.net>
2020-03-02 22:09:03 +0200

    Update shooper, added model swift class.

---
+++ window-shooper/Model/Wage.swift
@@ -0,0 +1,14 @@
+//
+//  Wage.swift
+//  window-shooper
+//
+//  Created by admin on 02.03.20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+class Wage {
+    class func getHours (forWage wage: Double, andPrice price: Double) -> Int {
+        
+    }
+}
